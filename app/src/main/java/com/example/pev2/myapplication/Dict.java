21041496fc492181edc8604e2c3edf47f5f35c36
Yauheni.Pashkelevich <Y.Pashkelevich@mps-solutions.de>
2018-12-29 22:34:00 +0300

    доработал немного. пробую запустить

---
+++ app/src/main/java/com/example/pev2/myapplication/Dict.java
@@ -0,0 +1,27 @@
+package com.example.pev2.myapplication;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Dict {
+
+    private final StringBuilder mDict = new StringBuilder();
+    private final Set<String> mWords = new HashSet<String>();
+
+    private static final String NL = System.getProperty("line.separator");
+
+    public void add(String key, String value) {
+        if (! mWords.contains(key)) {
+            mDict.append(key);
+            mDict.append("  "); // two spaces
+            mDict.append(value);
+            mDict.append(NL);
+            mWords.add(key);
+        }
+    }
+
+    public String toString() {
+        return mDict.toString();
+    }
+
+}
--- app/src/main/java/com/example/pev2/myapplication/MainActivity.java
+++ app/src/main/java/com/example/pev2/myapplication/MainActivity.java
@@ -1,39 +1,39 @@
 package com.example.pev2.myapplication;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+import edu.cmu.pocketsphinx.SpeechRecognizer;
+import edu.cmu.pocketsphinx.SpeechRecognizerSetup;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private SpeechRecognizer mRecognizer;
+
+    private static final String KWS_SEARCH = "hotword";
+    private static final String COMMAND_SEARCH = "command";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d("myTag", "---onClick");
-            }
-        });
-
+        findViewById(R.id.speak).setOnClickListener(this);
         Log.d("myTag", "---onCreate");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         Log.d("myTag", "---onCreateOptionsMenu");
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -41,16 +41,70 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Log.d("myTag", "---onOptionsItemSelected");
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    public void listen() throws IOException {
+        Log.d("MainActivity", "speaklog listen start");
+        String[] names = (String[]) Arrays.asList("да", "нет").toArray();
+        final String hotword = "умный дом";
+
+        PhonMapper phonMapper = new PhonMapper(getAssets().open("dict/ru/hotwords"));
+        Grammar grammar = new Grammar(names, phonMapper);
+        grammar.addWords(hotword);
+        DataFiles dataFiles = new DataFiles(getPackageName(), "ru");
+        File hmmDir = new File(dataFiles.getHmm());
+        File dict = new File(dataFiles.getDict());
+        File jsgf = new File(dataFiles.getJsgf());
+        copyAssets(hmmDir);
+        saveFile(jsgf, grammar.getJsgf());
+        saveFile(dict, grammar.getDict());
+        mRecognizer = SpeechRecognizerSetup.defaultSetup()
+                .setAcousticModel(hmmDir)
+                .setDictionary(dict)
+                .setBoolean("-remove_noise", false)
+                .setKeywordThreshold(1e-7f)
+                .getRecognizer();
+        mRecognizer.addKeyphraseSearch(KWS_SEARCH, hotword);
+        mRecognizer.addGrammarSearch(COMMAND_SEARCH, jsgf);
+        Log.d("MainActivity", "speaklog listen end");
+    }
+
+    private void copyAssets(File baseDir) throws IOException {
+        String[] files = getAssets().list("hmm/ru");
+
+        for (String fromFile : files) {
+            File toFile = new File(baseDir.getAbsolutePath() + "/" + fromFile);
+            InputStream in = getAssets().open("hmm/ru/" + fromFile);
+            FileUtils.copyInputStreamToFile(in, toFile);
+        }
+    }
+
+    private void saveFile(File f, String content) throws IOException {
+        File dir = f.getParentFile();
+        if (!dir.exists() && !dir.mkdirs()) {
+            throw new IOException("Cannot create directory: " + dir);
+        }
+        FileUtils.writeStringToFile(f, content, "UTF8");
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.speak:
+                Log.d("MainActivity", "speaklog onClick");
+                try {
+                    listen();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
     }
 }
